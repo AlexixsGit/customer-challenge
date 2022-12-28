6257ee079b367456f7e08c5784d7f3df535cbaac
Alexander Pabon <alex312-@hotmail.com>
2022-12-28 11:18:47 -0500

    implement bean UseCaseConfig

--- applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
@@ -1,9 +1,16 @@
 package co.com.bancolombia.config;
 
+import co.com.bancolombia.model.customer.gateways.CustomerRepository;
+import co.com.bancolombia.usecase.customer.CustomerUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 public class UseCasesConfig {
+    @Bean
+    public CustomerUseCase customerUseCaseConfig(CustomerRepository customerRepository) {
+        return new CustomerUseCase(customerRepository);
+    }
 }
